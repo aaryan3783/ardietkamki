3bb33a9fb66bb38fdaf3bef36336489dddd1e104
Aaryan <124796350+aaryan3783@users.noreply.github.com>
2024-01-06 04:08:18 +0530

    Add files via upload

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+
+  const nav = document.getElementById("nav")
+  const hamburger = document.getElementById("hamburger-icon")
+
+  hamburger.addEventListener("click", ()=>{
+    nav.classList.toggle("active-toggle")
+  })
+
+  
